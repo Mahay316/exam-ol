702e847fc50b604a8f8eb2b286dfc9758ac3d4a7
Mahay <pengfeizhao316@gmail.com>
2020-12-13 22:35:05 +0800

    导出requirements.txt以统一运行环境

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ app.config.from_pyfile('config.py')
 # 全局错误处理
 
 
-# 使用pycharm运行flask环境时不能加__name__ == '__main__'的判断
+if __name__ == '__main__':
+    # 为了避免循环引用，延时初始化数据库
+    app.app_context().push()
+    init_db(app)
 
-# 为了避免循环引用，延时初始化数据库
-app.app_context().push()
-init_db(app)
-
-# 在此处注册蓝图
-# app.register_blueprint(auth)
-app.register_blueprint(exam_bp, url_prefix='/exam')
+    # 在此处注册蓝图
+    app.register_blueprint(exam_bp, url_prefix='/exam')
+    app.run(debug=True)
