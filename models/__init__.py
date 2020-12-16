84b290cb6eb259c52bf89eae7b9ac5f253488f7d
B5DX <wrwzx1@163.com>
2020-12-16 17:43:57 +0800

    重构数据库部分，消除重复引用和拼写错误

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,6 @@ from .database import init_db
 from .TestModel import Test
 from .PaperModel import Paper
 from .QuestionModel import Question
-from .QuesrionPaperModel import QuestionPaper
+from .QuestionPaperModel import QuestionPaper
 from .UserModel import User, Mentor, Student, Admin
-from .TestModel import Test
-from .PaperModel import Paper
-from .QuestionModel import Question
-from .QuesrionPaperModel import QuestionPaper
 from .StudentTestModel import StudentTest
