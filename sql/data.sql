990e880534577b206714ff03276edaf8695a70bc
Mahay <53511138+Mahay316@users.noreply.github.com>
2020-12-29 15:26:13 +0800

    修改数据库结构，实现存储过程 (#11)
    * 显式指定数据库编码，以避免字符串比较时出现问题
    
    * 修改了建表语句exam.sql
    
    1. 更改question表的Qselect字段定义，表示选择题的备选项
    2. 更改question表的Qanswer字段定义，表示题目答案
    3. 重新定义Qselect和Qanswer字段格式为JSON数组
    
    * 将所有表的主键修改为自增的int
    
    * 修改数据库结构
    
    1. 除学生、教师和管理员表外，其他表的主键改为int自增
    2. 修改data.sql适应表结构的变化
    
    * 实现了存储过程，接管试题和试卷的更新/删除功能
    
    * 删除question_paper表中的题目位置字段，因为前端目前无法实现记录题目顺序
    
    * 在README.md中更新运行项目的步骤

--- sql/data.sql
+++ sql/data.sql
@@ -22,46 +22,46 @@ INSERT INTO `student`(`Sno`, `Sname`, `Sgender`, `Smajor`, `Spassword`) VALUES (
 INSERT INTO `student`(`Sno`, `Sname`, `Sgender`, `Smajor`, `Spassword`) VALUES ('student4', 'Антон', '男', '核工程', '123456');
 INSERT INTO `student`(`Sno`, `Sname`, `Sgender`, `Smajor`, `Spassword`) VALUES ('student4_en', 'Антон', '男', '核工程', 'e10adc3949ba59abbe56e057f20f883e');
 
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0001', '软件工程');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0002', '数据库原理');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0003', '编译原理');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0004', '流体力学');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0005', '工程热力学');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0006', 'C语言程序设计');
-INSERT INTO `subject`(`Subno`, `Subname`) VALUES ('s0007', 'Java高级语言程序设计');
+INSERT INTO `subject`(`Subname`) VALUES ('软件工程');
+INSERT INTO `subject`(`Subname`) VALUES ('数据库原理');
+INSERT INTO `subject`(`Subname`) VALUES ('编译原理');
+INSERT INTO `subject`(`Subname`) VALUES ('流体力学');
+INSERT INTO `subject`(`Subname`) VALUES ('工程热力学');
+INSERT INTO `subject`(`Subname`) VALUES ('C语言程序设计');
+INSERT INTO `subject`(`Subname`) VALUES ('Java高级语言程序设计');
 
-INSERT INTO `course`(`Cno`, `Cname`, `Subno`, `Mno`) VALUES ('c0001', '数据库教学班1', 's0002', 'teacher1');
-INSERT INTO `course`(`Cno`, `Cname`, `Subno`, `Mno`) VALUES ('c0002', '数据库教学班2', 's0002', 'teacher1');
-INSERT INTO `course`(`Cno`, `Cname`, `Subno`, `Mno`) VALUES ('c0003', '数据库教学班3', 's0002', 'teacher1_en');
-INSERT INTO `course`(`Cno`, `Cname`, `Subno`, `Mno`) VALUES ('c0004', '流体力学1', 's0004', 'teacher2');
-INSERT INTO `course`(`Cno`, `Cname`, `Subno`, `Mno`) VALUES ('c0005', '流体力学2', 's0004', 'teacher2_en');
+INSERT INTO `course`(`Cname`, `Subno`, `Mno`) VALUES ('数据库教学班1', 2, 'teacher1');
+INSERT INTO `course`(`Cname`, `Subno`, `Mno`) VALUES ('数据库教学班2', 2, 'teacher1');
+INSERT INTO `course`(`Cname`, `Subno`, `Mno`) VALUES ('数据库教学班3', 2, 'teacher1_en');
+INSERT INTO `course`(`Cname`, `Subno`, `Mno`) VALUES ('流体力学1', 4, 'teacher2');
+INSERT INTO `course`(`Cname`, `Subno`, `Mno`) VALUES ('流体力学2', 4, 'teacher2_en');
 
-INSERT INTO `paper`(`Pno`, `Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('p0001', '数据库基础卷', 3, 17, 0, 0, 's0002');
-INSERT INTO `paper`(`Pno`, `Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('p0002', '数据库提高卷', 0, 0, 0, 0, 's0002');
-INSERT INTO `paper`(`Pno`, `Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('p0003', '流体力学基础卷', 1, 4, 0, 1, 's0004');
-INSERT INTO `paper`(`Pno`, `Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('p0004', '软件工程提高卷', 0, 0, 0, 0, 's0001');
+INSERT INTO `paper`(`Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('数据库基础卷', 3, 17, 0, 0, 2);
+INSERT INTO `paper`(`Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('数据库提高卷', 0, 0, 0, 0, 2);
+INSERT INTO `paper`(`Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('流体力学基础卷', 1, 4, 0, 1, 4);
+INSERT INTO `paper`(`Pname`, `Pnum`, `Pscore`, `Preference`, `Pisdeleted`, `Subno`) VALUES ('软件工程提高卷', 0, 0, 0, 0, 1);
 
-INSERT INTO `question`(`Qno`, `Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('q00001', 'select', '这是一道数据库单选题()', '{\"A\": \"我是A\", \"B\": \"我是B\", \"C\": \"我是C\". \"D\": \"我是D\"}', 'A', 's0002', 0, 1);
-INSERT INTO `question`(`Qno`, `Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('q00002', 'multi', '这是一道流体力学多选题()', '{\"A\": \"我是A\", \"B\": \"我是B\", \"C\": \"我是C\". \"D\": \"我是D\", \"E\": \"我是E\"}', 'ABC', 's0001', 0, 0);
-INSERT INTO `question`(`Qno`, `Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('q00003', 'fill', '这是一道[填空]填空[填空]', '{\"A\": \"编译原理\", \"B\": \"题\"}', NULL, 's0003', 0, 0);
+INSERT INTO `question`(`Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('select', '这是一道数据库单选题()', '{\"A\": \"我是A\", \"B\": \"我是B\", \"C\": \"我是C\". \"D\": \"我是D\"}', 'A', 2, 0, 1);
+INSERT INTO `question`(`Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('multi', '这是一道流体力学多选题()', '{\"A\": \"我是A\", \"B\": \"我是B\", \"C\": \"我是C\". \"D\": \"我是D\", \"E\": \"我是E\"}', 'ABC', 1, 0, 0);
+INSERT INTO `question`(`Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`, `Qreference`, `Qisdeleted`) VALUES ('fill', '这是一道[填空]填空[填空]', '{\"A\": \"编译原理\", \"B\": \"题\"}', NULL, 3, 0, 0);
 
-INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`, `QPposition`) VALUES ('p0001', 'q00001', 2, 1);
-INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`, `QPposition`) VALUES ('p0001', 'q00002', 10, 3);
-INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`, `QPposition`) VALUES ('p0001', 'q00003', 5, 2);
-INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`, `QPposition`) VALUES ('p0003', 'q00002', 4, 1);
+INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`) VALUES (1, 1, 2);
+INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`) VALUES (1, 2, 10);
+INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`) VALUES (1, 3, 5);
+INSERT INTO `question_paper`(`Pno`, `Qno`, `QPscore`) VALUES (3, 2, 4);
 
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0001', 'student1');
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0001', 'student1_en');
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0004', 'student2');
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0001', 'student3');
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0004', 'student3');
-INSERT INTO `student_course`(`Cno`, `Sno`) VALUES ('c0003', 'student4_en');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (1, 'student1');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (1, 'student1_en');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (4, 'student2');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (1, 'student3');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (4, 'student3');
+INSERT INTO `student_course`(`Cno`, `Sno`) VALUES (3, 'student4_en');
 
-INSERT INTO `test`(`Tno`, `Tname`, `Tdesc`, `Tstart`, `Tend`, `Pno`, `Cno`) VALUES ('t0001', '数据库期末考试', NULL, '2020-12-15 15:57:42', '2020-12-15 15:57:42', 'p0002', 'c0001');
-INSERT INTO `test`(`Tno`, `Tname`, `Tdesc`, `Tstart`, `Tend`, `Pno`, `Cno`) VALUES ('t0003', '流体力学期中考试', '考试说明测试','2020-12-18 15:00:00', '2020-12-19 15:00:00', 'p0003', 'c0004');
+INSERT INTO `test`(`Tname`, `Tdesc`, `Tstart`, `Tend`, `Pno`, `Cno`) VALUES ('数据库期末考试', NULL, '2020-12-15 15:57:42', '2020-12-15 15:57:42', 2, 1);
+INSERT INTO `test`(`Tname`, `Tdesc`, `Tstart`, `Tend`, `Pno`, `Cno`) VALUES ('流体力学期中考试', '考试说明测试','2020-12-18 15:00:00', '2020-12-19 15:00:00', 3, 4);
 
-INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES ('t0001', 'student1', NULL);
-INSERT INTO `student_test`(`Tno`, `Sno`, `STwrong`, `STblank`, `STgrade`) VALUES ('t0001', 'student1_en', 0, 0, 95);
-INSERT INTO `student_test`(`Tno`, `Sno`, `STwrong`, `STblank`, `STgrade`) VALUES ('t0001', 'student3', 1, 1, 78);
-INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES ('t0003', 'student2', NULL);
-INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES ('t0003', 'student3', NULL);
+INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES (1, 'student1', NULL);
+INSERT INTO `student_test`(`Tno`, `Sno`, `STwrong`, `STblank`, `STgrade`) VALUES (1, 'student1_en', 0, 0, 95);
+INSERT INTO `student_test`(`Tno`, `Sno`, `STwrong`, `STblank`, `STgrade`) VALUES (1, 'student3', 1, 1, 78);
+INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES (2, 'student2', NULL);
+INSERT INTO `student_test`(`Tno`, `Sno`, `STgrade`) VALUES (2, 'student3', NULL);
---
+++ sql/procs.sql
@@ -0,0 +1,81 @@
+DELIMITER $
+/*
+ * 存储过程：删除试题，根据是否被引用选择直接删除还是隐藏
+ * 输入：target 待删除的试题号
+ * 输出：res 若待删除的试题号不存在，返回FALSE，否则返回TRUE
+ * ### MySQL中BOOLEAN对应TINYINT(1)
+ */
+CREATE PROCEDURE delete_question(IN target VARCHAR(20), OUT res BOOLEAN)
+BEGIN
+	DECLARE ref_count INT;
+	SELECT Qreference INTO ref_count FROM question WHERE Qno = target;
+	IF ref_count IS NULL THEN
+		SET res = FALSE;
+	ELSE
+		IF ref_count > 0 THEN
+			UPDATE question SET Qisdeleted = TRUE WHERE Qno = target;
+		ELSE
+			DELETE FROM question WHERE Qno = target;
+		END IF;
+		SET res = TRUE;
+	END IF;
+END;$
+
+/*
+ * 存储过程：修改试题，根据是否被引用选择直接修改还是隐藏原题后插入新题
+ * 输入：target  待删除的试题号
+		 stem    题干
+		 answer  题目答案
+		 _select 选择题备选项，填空题此字段为NULL
+		 subject 题目所属科目
+   ### 虽然有可能某些字段并未被修改，但也需要完整地传进来
+ * 输出：res 若待更新的试题号不存在，返回FALSE，否则返回TRUE
+ * ### MySQL中BOOLEAN对应TINYINT(1)
+ */
+CREATE PROCEDURE update_question(IN target VARCHAR(20), IN stem VARCHAR(255), IN answer VARCHAR(255),
+	IN _select VARCHAR(255), IN subject VARCHAR(5), OUT res BOOLEAN)
+BEGIN
+	DECLARE ref_count INT;
+	DECLARE type ENUM('select', 'multi', 'fill');
+
+	SELECT Qreference INTO ref_count FROM question WHERE Qno = target;
+	-- ref_count为NULL说明查不到此条记录
+	IF ref_count IS NULL THEN
+		SET res = FALSE;
+	ELSE
+		IF ref_count > 0 THEN
+			-- 被引用了，隐藏原题创建一个新题
+			SELECT Qtype INTO type FROM question WHERE Qno = target;
+			UPDATE question SET Qisdeleted = TRUE WHERE Qno = target;
+			INSERT INTO `question`(`Qtype`, `Qstem`, `Qanswer`, `Qselect`, `Subno`)
+				VALUES (type, stem, answer, _select, subject);
+		ELSE
+			UPDATE question SET Qstem = stem, Qanswer = answer, Qselect = _select, Qsubject = subject
+				WHERE Qno = target;
+		END IF;
+		SET res = TRUE;
+	END IF;
+END;$
+
+/*
+ * 存储过程：删除试卷，根据是否被引用选择直接删除还是隐藏
+ * 输入：target 待删除的试卷号
+ * 输出：res 若待删除的试卷号不存在，返回FALSE，否则返回TRUE
+ * ### MySQL中BOOLEAN对应TINYINT(1)
+ */
+CREATE PROCEDURE delete_paper(IN target VARCHAR(20), OUT res BOOLEAN)
+BEGIN
+	DECLARE ref_count INT;
+	SELECT Preference INTO ref_count FROM paper WHERE Pno = target;
+	IF ref_count IS NULL THEN
+		SET res = FALSE;
+	ELSE
+		IF ref_count > 0 THEN
+			UPDATE paper SET Pisdeleted = TRUE WHERE Pno = target;
+		ELSE
+			DELETE FROM paper WHERE Pno = target;
+		END IF;
+		SET res = TRUE;
+	END IF;
+END;$
+DELIMITER ;
